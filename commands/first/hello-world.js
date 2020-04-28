a5a2d9e8205b8a95b4561418492f83f79daeb69e
Jeroen Kortekaas <jeroenkortekaas1@gmail.com>
2020-04-28 16:08:23 +0200

    get mod role

--- commands/first/hello-world.js
+++ commands/first/hello-world.js
@@ -3,14 +3,18 @@ const { Command } = require('discord.js-commando');
 module.exports = class HelloWorldCommand extends Command {
     constructor(client) {
 		super(client, {
-			name: 'hello-world',
+			name: 'h',
 			group: 'first',
-			memberName: 'hello-world',
+			memberName: 'h',
 			description: 'Replies with a hello world.',
 		});
 	}
 
 	run(message) {
+		let role =message.guild.roles.find('name', 'Moderator')
+		if(message.member._roles.find(r=>r === role.id)){
+		return message.say('Im a mod')
+	}
 		return message.say('Hello World! My name is bot');
 	}
 };
